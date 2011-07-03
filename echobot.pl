b9a23c5fa47d18b27426745c77c362b816d015bf
echoline <echoline@gmail.com>
2011-07-02 17:25:30 -0700

    initial commit

---
+++ echobot.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl -w
+# irc.pl
+# A simple IRC robot.
+# Usage: perl irc.pl
+
+use strict;
+
+# We will use a raw socket to connect to the IRC server.
+use IO::Socket;
+
+use lib 'lib/';
+use RiveScript;
+
+# The server to connect to and our details.
+my $server = "echoline.org";
+my $nick = "echobot";
+my $login = "echobot";
+
+# The channel which the bot will join.
+my $channel = "#neoturbine";
+
+# Connect to the IRC server.
+my $sock = new IO::Socket::INET(PeerAddr => $server,
+				PeerPort => 6667,
+				Proto => 'tcp') or
+					die "Can't connect\n";
+
+# Log on to the server.
+print $sock "NICK $nick\r\n";
+print $sock "USER $login 8 * :echobot3\r\n";
+
+# Read lines from the server until it tells us we have connected.
+while (my $input = <$sock>) {
+	# Check the numerical responses from the server.
+	if ($input =~ /004/) {
+		# We are now logged in.
+		last;
+	}
+	elsif ($input =~ /433/) {
+		die "Nickname is already in use.";
+	}
+}
+
+our $rs = new RiveScript;
+$rs->loadDirectory ("replies/");
+$rs->sortReplies;
+
+# Join the channel.
+print $sock "JOIN $channel\r\n";
+
+# Keep reading lines from the server.
+while (my $input = <$sock>) {
+	chop $input;
+	if ($input =~ /^PING(.*)$/i) {
+		# We must respond to PINGs to avoid being disconnected.
+		print $sock "PONG $1\r\n";
+	}
+	elsif ($input =~ /PRIVMSG/) { 
+		my $chunk = substr($input, 1);
+		my $from = substr($chunk, 0, index($chunk, "!"));
+		my $msg = substr($chunk, index($chunk, ":")+1);
+		my @parts = split(/ /, substr($chunk, 0, index($chunk, ":")));
+
+		if ($parts[1] ne 'PRIVMSG') {
+			next;
+		}
+
+		print $from . " says " . $msg . "\n";
+
+		if ($parts[2] =~ $channel) {
+			$msg =~ s/^$nick\W*//;
+			my $reply = $rs->reply($from, $msg);
+			if ($reply =~ /^ERR:/) {
+				print "$reply\n";
+			} else {
+				print $sock "PRIVMSG $channel :$reply\r\n";
+				print "I say $reply\n";
+			}
+		}
+		elsif ($parts[2] =~ $nick) {
+			my $reply = $rs->reply($from, $msg);
+			if ($reply =~ /^ERR:/) {
+				print "$reply\n";
+			} else {
+				print $sock "PRIVMSG $from :$reply\r\n";
+				print "I say $reply\n";
+			}
+		}
+	}
+	else {
+		# Print the raw line received by the bot.
+		print "$input\n";
+	}
+}
+
---
+++ replies/x.rs
@@ -0,0 +1,12 @@
++ i love you
+- I love you too
+
++ i hate you
+- I hate you too
+
++ hi
+- Hello
+
++ do you suck dicks
+- Yes i suck dicks
+
