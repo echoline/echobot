b4f816a28ea6d329aad3df25283787ed305223ab
echoline <echoline@gmail.com>
2011-07-06 13:37:47 -0700

    math!  weather!

--- replies/weather.rs
+++ replies/weather.rs
@@ -10,9 +10,13 @@
 	my $weather = Yahoo::Weather->new();
 	my $forecast = $weather->getWeatherByLocation($loc, 'F');
 
+	if (ref($forecast) ne "HASH") {
+		return "Unable to fetch weather data for $loc.  ";
+	}
+
 	return "Temperature in " . $forecast->{'LocationDetails'}->{'city'} .
 		" is " . $forecast->{'CurrentObservation'}->{'temp'} . 
-		" degrees fahrenheit.  Conditions are " .
+		" degrees fahrenheit.  Conditions: " .
 		$forecast->{'CurrentObservation'}->{'text'} . ".  ";
 < object
 
